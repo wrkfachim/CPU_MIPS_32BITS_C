28d0fa1d2360a7cda573ca6e7c1d40291f272faf
werik <werikfachim@gmail.com>
2018-05-30 15:59:43 -0300

    Funcao main modificada aqdequada ao problema!

--- main.c
+++ main.c
@@ -1,349 +1,214 @@
+/* Arquivo cpu_mips_multiciclo_2018.c
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int memoria[];
-extern int reg[];
-extern char loop;
-extern void ula (int a, int b, char ula_op, int *result_ula, char *zero, char *overflow);
+#define MAX 1024
+#define NUMREG 32
 
-char returnSignal = 0;
+/***********************
+prototipacao inicial
+ ***********************/
 
-//variavel pra testes
-char pausa;
+int main (int, char **);
 
-void UnidadeControle(int IR, int *sc){
+/* ULA segue a especifica��o dada em sala de aula.
+void ula (int a, int b, char ula_op, int *result_ula, char *zero, char *overflow)
+args de entrada:		int a, int b, char ula_op
+args de saida: 		int *result_ula, char *zero, char *overflow */
+void ula(int, int, char, int *, char *, char *);
 
-	int opcode = IR >> 26; 
-	opcode = opcode&0x3f; //filtrando para tratar negativo
+/* UC principal
+void UnidadeControle(int IR, int *sc);
+args de entrada:		 int IR
+args de saida: 		int *sc     */
+void UnidadeControle(int, int *);
 
-	switch(*sc) {
-		
-		case 0:				 //Inicializador
-			*sc = 0x44020;	 //Estado 0
+/* Busca da Instrucao
+void Busca_Instrucao(int sc, int PC, int ALUOUT, int IR, int A, int B, int *PCnew, int *IRnew, int *MDRnew);
+args de entrada:		int sc, int PC, int ALUOUT, int IR, int A, int B
+args de saida: 		int *PCnew, int *IRnew, int *MDRnew     */
+void Busca_Instrucao(int, int, int, int, int, int, int*, int*, int*);
+
+/* Decodifica Instrucao, Busca Registradores e Calcula Endereco para beq
+void Decodifica_BuscaRegistrador(int sc, int IR, int PC, int A, int B, int *Anew, int *Bnew, int *ALUOUTnew);
+args de entrada:		int sc, int IR, int PC, int A, int B,
+args de saida: 		int *Anew, int *Bnew, int *ALUOUTnew     */
+void Decodifica_BuscaRegistrador(int, int, int, int, int, int *, int *, int *);
+
+/*Executa TipoR, Calcula endereco para lw/sw e efetiva desvio condicional e incondicional
+void Execucao_CalcEnd_Desvio(int sc, int A, int B, int IR, int PC, int ALUOUT, int *ALUOUTnew, int *PCnew);
+args de entrada:		int sc, int A, int B, int IR, int PC, int ALUOUT
+args de saida: 		 int *ALUOUTnew, int *PCnew */
+void Execucao_CalcEnd_Desvio(int, int, int, int, int, int, int *, int *);
+
+/* Escreve no Bco de Regs resultado TiporR, Le memoria em lw e escreve na memoria em sw
+void EscreveTipoR_AcessaMemoria(int sc, int B, int IR, int ALUOUT, int PC, int *MDRnew, int *IRnew);
+args de entrada:	 int sc, int B, int IR, int ALUOUT, int PC
+args de saida: 	 int *MDRnew, int *IRnew */
+void EscreveTipoR_AcessaMemoria(int, int, int, int, int, int *, int *);
+
+/* Escreve no Bco de Regs o resultado da leitura da memoria feita por lw
+void EscreveRefMem(sort int sc, int IR, int MDR, int ALUOUT);
+args de entrada:		int sc, int IR, int MDR, int ALUOUT
+args de saida: 		nao ha */
+void EscreveRefMem(int, int, int, int);
+
+
+/****************************
+definicao de variaveis globais
+ ****************************/
+int memoria[MAX];		// Memoria RAM
+int reg[NUMREG];		// Banco de Registradores
+
+char loop = 1; 		// variavel auxiliar que determina a parada da execucao deste programa
+
+void ula (int a, int b, char ula_op, int *result_ula, char *zero, char *overflow)
+{
+	*overflow = 0;
+	switch (ula_op)
+	{
+	  	case 0:    // and
+	  		*result_ula = a & b;
+	  	break;
+
+	 	case 1:   // or
+	  		*result_ula = a | b;
+	  	break;
+
+		case 2:  // add
+			*result_ula = a + b;
+			if ( (a >= 0 && b >= 0 && *result_ula < 0) || (a < 0 && b < 0 && *result_ula >= 0) )
+				*overflow  = 1;
 		break;
 
-		case 0x44020:		 //Estado 0    
-			*sc = 0x30;		 //Estado 1  ... Estado0->Estado1
-		break;
+	  	case 6:   // sub
+	  		*result_ula = a - b;
+	  		if ( (a >= 0 && b < 0 && *result_ula < 0) || (a < 0 && b >= 0 && *result_ula >= 0) )
+				*overflow  = 1;
+	  	break;
 
-		case 0x30:			//Estado 1
-			
-			/*
+	  	case 7:   // slt
+	  		if(a < b)
+				*result_ula = 1;
+	  		else
+				*result_ula = 0;
+	  	break;
 
-			Tabela de despacho(fetch) de acordo com o microcódigo 
-			
-			decode 		     (2)
-			exec   		     (6)
-			branch 		     (8)
-			jump   		 	 (9)								  
-			bne		0x05	 (10)
-			bltz	0x01	 (11)
-			move	0X3F	 (12)
-			lui		0X0F	 (13)
-			ori		0X0D	 (14)
-			andi	0X0C	 (15)
-			slti	0X0A	 (16)
-			
-			*/
-		
-			if(opcode == 0){
-				*sc = 0x88;	//Estado 6 - Se for Tipo-R    --- Estado1 -> Estado6
-			}
-			else if(opcode == 0x2B || opcode == 0x23){
-				*sc = 0x18;	//Estado 2 - Se for lw/sw  	   ---- Estado1 -> Estado2
-			}
-			else{
-				switch(opcode){
-					case 0x4:	
-						*sc = 0xD0C;	//Estado 8 beq 	   --- Estado1 -> Estado8
-						break;
+	  	case 12:   // nor
+	  		*result_ula = ~(a | b);
+	  	break;
+  	}
+  	if (*result_ula == 0)
+		*zero = 1;
+  	else
+		*zero = 0;
 
-					case 0x2:	
-						*sc = 0x1A00;	//Estado 9 jump(j) --- Estado1 -> Estado9
-						break;
-
-					case 0x5:	
-						*sc = 0xD08;	//Estado 10 bne    --- Estado1 -> Estado10
-						break;
-
-					case 0x1:	
-						*sc = 0xCF4;	//Estado 11 bltz   --- Estado1 -> Estado11
-						break;
-
-					case 0x3f:	
-						*sc = 0x10002;	//Estado 12 move   --- Estado1 -> Estado12
-						break;
-
-					case 0x0f:	
-						*sc = 0x30002;	//Estado 13 lui    --- Estado1 -> Estado13
-						break;
-
-					case 0x0d:	
-						*sc = 0x58;	    //Estado 14 ori	   --- Estado1 -> Estado14
-						break;
-
-					case 0x0c:	
-						*sc = 0x198;	//Estado 15 andi   --- Estado1 -> Estado15
-						break;
-
-					case 0x0a:	
-						*sc = 0x158;	//Estado 16 slti   ---Estado1 -> Estado16
-						break;
-				}
-				
-			}
-		break;
-
-		case 0x18:	//Estado2 (lw ou sw)
-			/*
-			
-			Tabela de despacho(fetch) de acordo com o microcódigo 
-
-				lw 	0x2B 	(3)
-				sw 	0x23 	(5)
-				
-			*/	
-			if(opcode == 0x23) {
-				*sc = 0x6000;	//Estado2 -> Estado3
-			}
-			else {
-				*sc = 0xA000;	//Estado2 -> Estado5
-			}
-			break;
-
-		case 0x6000:	
-			*sc = 0x20002; 	   //Estado3 -> Estado4
-			break;
-
-		case 0x20002:	
-			*sc = 0x44020;	   //Estado4 -> Estado0
-			break;
-
-		case 0xA000:	
-			*sc = 0x44020;	   //Estado5 -> Estado0
-			break;
-
-		case 0x88:			   //Estado6 -> Estado7
-			*sc = 0x3;	
-			break;
-
-		case 0x3:	
-			*sc = 0x44020;	   //Estado7 -> Estado0
-			break;
-
-		case 0xD0C:		
-			*sc = 0x44020;    //Estado8 -> Estado0
-			break;
-
-		case 0x1A00:	 
-			*sc = 0x44020;	  //Estado9 -> Estado0
-			break;
-
-		case 0xD08:	
-			*sc = 0x44020;	 //Estado10 -> Estado0
-			break;
-
-		case 0xCF4:	
-			*sc = 0x44020;	////Estado11 -> Estado0
-			break;
-
-		case 0x10002:	
-			*sc = 0x44020;	//Estado12 -> Estado0
-			break;
-
-		case 0x30002:		//Estado13 -> Estado0
-			*sc = 0x44020;	
-			break;
-
-		case 0x58:		   //Estado14 -> Estado7
-			*sc = 0x3;	
-			break;
-
-		case 0x198:	      //Estado15 -> Estado7
-			*sc = 0x3;	
-			break;
-
-		case 0x158:	     //Estado16 -> Estado7
-			*sc = 0x3;	
-			break;
-
-	}
-
-	//teste
-	printf("estado: %d\n", *sc);
+  return;
 }
 
-void Busca_Instrucao(int sc, int PC, int ALUOUT, int IR, int A, int B, int *PCnew, int *IRnew, int *MDRnew){
-	
-	char zero, overflow;
-	if((sc & 0x40000) !=0) {			 //Se IRWrite == 1
-		if((sc & 0x2000) == 0) {		 //Se IorD == 0, a memória é acessada na posição de PC
-			*IRnew = memoria[PC/4];
+// contem todas as funcoes desenvolvidas por voce para o processador multiciclo.
+//Inclua aqui as suas funcoes/procedimentos
+#include "cpu_multi_code.c"
+
+int main (int argc, char *argv[])
+{
+	int PCnew = 0, IRnew, MDRnew, Anew, Bnew, ALUOUTnew;
+					// Registradores auxiliares usados para a escrita dentro de um ciclo.
+					// Guardam temporariamente o resultado durante um ciclo. Os resultados aqui armazenados estarao
+					// disponiveis para leitura no final do ciclo atual (para que o mesmo esteja disponivel apenas no
+					// incio do ciclo seguinte).
+					// Em um ciclo sempre e lido o conteudo de um registrador atualizado no ciclo anterior.
+
+	int PC = 0, IR=-1, MDR, A, B, ALUOUT;
+					// Registradores especiais usados para a leitura em um ciclo.
+					// Guardam os resultados que poderao ser utilizados ja neste ciclo, pois foram atualizados no final
+					// do ciclo anterior.
+					// Ex.: em um ciclo, o resultado da ULA e inserido inicialmente em ALUOUTnew.  Apenas no final
+					// do ciclo esse conteudo podera ser atribuido para ALUOUT, para que o mesmo possa ser
+					// usado no ciclo seguinte.
+					// Em um ciclo sempre e lido o conteudo de um registrador atualizado no ciclo anterior.
+
+	int sc = 0;	   	// Sinais de Controle
+					//                    cada bit determina um dos sinais de controle que saem da UC.
+					// A posicao de cada sinal dentro do int esta especificada no enunciado
+	char ula_op = 0;   			// Sinais de Controle de entrada para a ULA
+					//                    sao usados apenas os 4 bits menos significativos dos 8 disponiveis.
+
+	int nr_ciclos = 0; 			// contador do numero de ciclos executados
+
+/*
+As variaveis zero e overflow nao precisam definidas na main.
+Serao argumentos de retorno da ula e devem ser definidas localmente nas
+rotinas adequadas.
+char  zero, overflow;  			// Sinais de Controle de saida da UL: bit zero e bit para indicar overflow
+*/
+
+	memoria[0] = 0x8c480000;  // 1000 1100 01007 1000 0000 0000 0000 0000  lw $t0, 0($v0)								5
+	memoria[1] = 0x010c182a;  // 0000 0001 0000 1100 0001 1000 0010 1010   slt $v1, $t0, $t4								4
+	memoria[2] = 0x106d0004;  // 0001 0000 0110 1101 0000 0000 0000 0100  beq $v1, $t5, fim(4 palavras abaixo de PC+4)	3
+	memoria[3] = 0x01084020;  // 0000 0001 0000 1000 0100 0000 0010 0000  add $t0, $t0, $t0								4
+	memoria[4] = 0xac480000;  // 1010 1100 0100 1000 0000 0000 0000 0000  sw $t0, 0($v0)								4
+	memoria[5] = 0x004b1020;  // 0000 0000 0100 1011 0001 0000 0010 0000  add $v0, $t3, $v0								4
+	memoria[6] = 0x08000000;  // 0000 1000 0000 0000 0000 0000 0000 0000  j inicio (palavra 0)								3
+	memoria[7] = 0;           // fim (criterio de parada do programa)    (27*6)+(5+4+3)+1
+	memoria[8] = 0;
+	memoria[9] = 0;
+
+	// Dados
+	memoria[20] = 10;
+	memoria[21] = 12;
+	memoria[22] = 14;
+	memoria[23] = 16;
+	memoria[24] = 18;
+	memoria[25] = 20;
+	memoria[26] = -1;
+
+	reg[2]  = 80;  // $v0
+
+	reg[11] = 4;  // $t3
+	reg[12] = 0;  // $t4
+	reg[13] = 1;  // $t5
+
+	while(loop){
+		// aqui comeca um novo ciclo
+
+		// abaixo estao as unidades funcionais que executarao em todos os ciclos
+		// os sinais de controle em sc impedirao/permitirao que a execucao seja, de fato, efetivada
+
+		UnidadeControle(IR, &sc);
+		Busca_Instrucao(sc, PC, ALUOUT, IR, A, B, &PCnew, &IRnew, &MDRnew);
+		Decodifica_BuscaRegistrador(sc, IR, PC, A, B, &Anew, &Bnew, &ALUOUTnew);
+		Execucao_CalcEnd_Desvio(sc, A, B, IR, PC, ALUOUT, &ALUOUTnew, &PCnew);
+		EscreveTipoR_AcessaMemoria(sc, B, IR, ALUOUT, PC, &MDRnew, &IRnew);
+		EscreveRefMem(sc, IR, MDR, ALUOUT);
+
+		// contador que determina quantos ciclos foram executados
+		nr_ciclos++;
+
+		// atualizando os registradores temporarios necessarios ao proximo ciclo.
+		PC	 	= PCnew;
+		IR		= IRnew;
+		MDR		= MDRnew;
+		A		= Anew;
+		B		= Bnew;
+		ALUOUT 	= ALUOUTnew;
+
+		// aqui termina um ciclo
+	} // fim do while(loop)
+
+	// impressao da memoria para verificar se a implementacao esta correta
+	{
+		int ii;
+		for (ii = 20; ii < 27; ii++) {
+			printf("memoria[%d]=%d \n", ii, memoria[ii]);
 		}
-		else{							//Se IorD == 1, a memória é acessada na posição de ALUout
-			*IRnew = memoria[ALUOUT/4];
-		}
-		*MDRnew = *IRnew << 16;			//????
-		*MDRnew = *MDRnew >> 16;
-		ula(PC, 0x4, 0x2, PCnew, &zero, &overflow);
+		printf("Nr de ciclos executados =%d \n", nr_ciclos);
 	}
-}
+	exit(0);
+} 
 
-void Decodifica_BuscaRegistrador(int sc, int IR, int PC, int A, int B, int *Anew, int *Bnew, int *ALUOUTnew){
-	
-	char zero, overflow;
-	
-	if(sc==0x30) {	//Se Estado1 (decode)
-		if(IR == 0) {
-			loop = 0;
-			printf("HALT!\n");
-			returnSignal = 1;
-			return;
-		}
-		else {
-			*Anew = IR >> 21;		//*Anew recebe os 5 bits referentes ao registrador RS
-			*Anew = *Anew & 0x1f;
-
-			*Bnew = IR >> 16;		//*Anew recebe os 5 bits referentes ao registrador RT
-			*Bnew = *Bnew & 0x1f;
-
-			IR = IR << 16;
-			IR = IR >> 14;
-			ula(PC, IR, 0x2, ALUOUTnew, &zero, &overflow);	//Atualiza o valor de ALUOUT
-		}
-	}
-}
-
-void Execucao_CalcEnd_Desvio(int sc, int A, int B, int IR, int PC, int ALUOUT, int *ALUOUTnew, int *PCnew){
-
-	char zero, overflow;
-	int op;
-	
-	int VI = IR << 16;
-	VI = VI >> 16;
-	 
-	if(returnSignal == 1) {
-		return;
-	}
-
-	if(sc == 0x88){		//Se for tipo-r
-						
-		op = (IR & 0x3f);
-		switch(op) {
-			case 0x20:	//add
-				ula(reg[A], reg[B], 0x2, ALUOUTnew, &zero, &overflow);
-				break;
-
-			case 0x22:	//sub
-				ula(reg[A], reg[B], 0x6, ALUOUTnew, &zero, &overflow);
-				break;
-
-			case 0x24:	//and
-				ula(reg[A], reg[B], 0x0, ALUOUTnew, &zero, &overflow);
-				break;
-
-			case 0x25:	//or
-				ula(reg[A], reg[B], 0x1, ALUOUTnew, &zero, &overflow);
-				break;
-
-			case 0x2a:	//slt
-				ula(reg[A], reg[B], 0x7, ALUOUTnew, &zero, &overflow);
-				break;
-		}
-	}
-
-	else if(sc == 0x18) {	//lw ou sw
-		ula(reg[A], VI, 0x2, ALUOUTnew, &zero, &overflow);	//Atualiza o valor de ALUOUT
-	}
-
-	else if(sc == 0xCF4) {	//BLTZ
-		ula(reg[A], 0, 0x7, ALUOUTnew, &zero, &overflow);
-		
-		if(*ALUOUTnew == 1) {
-			*PCnew = ALUOUT;
-		}
-	}
-
-	else if(sc == 0xD0C) {	//BEQ
-		ula(reg[A], reg[B], 0x6, ALUOUTnew, &zero, &overflow);
-		
-		if(zero == 1) {
-			*PCnew = ALUOUT;
-		}
-	}
-
-	else if(sc == 0xD08) {	//BNE
-		ula(reg[A], reg[B], 0x6, ALUOUTnew, &zero, &overflow);
-		if(zero == 0) {
-			*PCnew = ALUOUT;
-		}
-	}
-
-	else if(sc == 0x1A00) {	//j
-		
-		VI = IR&0x3ffffff;	//realiza a extensão de sinal
-		VI = VI << 2;
-		*PCnew = VI;
-	}
-
-	else if(sc == 88) {		//ori
-		ula(reg[A], VI, 0x1, ALUOUTnew, &zero, &overflow);
-	}
-
-	else if(sc == 0x198) {	//andi
-		ula(reg[A], VI, 0x0, ALUOUTnew, &zero, &overflow);
-	}
-
-	else if(sc == 0x158) {	//slti
-		ula(reg[A], VI, 0x7, ALUOUTnew, &zero, &overflow);
-	}
-
-	else if(sc == 0x10002) {	//move
-		reg[A] = reg[B];
-	}
-
-	else if(sc == 0x30002) {	//lui
-		reg[B] = IR << 16;
-	}
-}
-
-void EscreveTipoR_AcessaMemoria(int sc, int B, int IR, int ALUOUT, int PC, int *MDRnew, int *IRnew){
-	int RD;
-	if(returnSignal==1) {
-		return;
-	}
-	if(sc == 3) {	//7
-		if(IR >> 27 == 0) {	//Tipo-R
-			RD = IR >> 11;
-			RD = RD&0x1f;
-			reg[RD] = ALUOUT;
-		}
-		else{	//Tipo-I
-			reg[B] = ALUOUT;
-		}
-		printf("\t\tresultado da ula: %d\n", ALUOUT);
-	}
-	else if(sc == 0x6000) {	//3
-		(*MDRnew) = (memoria[ALUOUT/4]) << 16;	//LW
-		(*MDRnew) = (*MDRnew) >> 16;	//LW
-		printf("----------ALUOUT: %d\n", ALUOUT);
-		printf("MDRnew: %d\n", *MDRnew);
-	}
-	else if(sc == 0xA000) {	//5
-		memoria[ALUOUT/4] = reg[B];	//SW
-	}
-}
-
-void EscreveRefMem(int sc, int IR, int MDR, int ALUOUT){
-	if(returnSignal == 1) {
-		return;
-	}
-	int B = IR >> 16;		//*Anew recebe os 5 bits referentes ao registrador RT
-	B = B & 0x1f;
-	if(sc == 0x20002) {
-		reg[B] = MDR;
-	}
-}
-
-
-
+// fim de main
+// ***********************************************************************************************
